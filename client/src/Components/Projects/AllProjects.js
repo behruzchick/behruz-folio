a1e24e395ee5302051c0f99d570d0891cbcfcce0
Hp <akbaralievbehruz44@gmail.com>
2024-03-20 21:53:34 +0500

    add modal

--- client/src/Components/Projects/AllProjects.js
+++ client/src/Components/Projects/AllProjects.js
@@ -2,37 +2,50 @@ export const projects = [
     {
         img:"https://i.imgur.com/PTVkoPv.png",
         title:'Instagram clone',
-        link:'https://lovely-begonia-033a8a.netlify.app/register'
+        link:'https://lovely-begonia-033a8a.netlify.app/register',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur laboriosam sunt nemo nulla aut itaque obcaecati ratione ex, iste animi illo veniam rerum rem sed optio officia suscipit minima tempora enim perferendis ipsa? Sapiente, reprehenderit maiores possimus optio maxime molestiae!',
+        gitHubLink:'https://github.com/behruzchick/insagram-client'
     },
     {
         img:"https://i.imgur.com/woth2Ob.png",
         title:'Codelabs.uz',
-        link:'https://codelabs-eight.vercel.app/'
+        link:'https://codelabs-eight.vercel.app/',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur laboriosam sunt nemo nulla aut itaque obcaecati ratione ex, iste animi illo veniam rerum rem sed optio officia suscipit minima tempora enim perferendis ipsa? Sapiente, reprehenderit maiores possimus optio maxime molestiae!',
+        gitHubLink:'https://github.com/behruzchick/codelabs'
     },
     {
         img:"https://i.imgur.com/3O1r1X9.png",
         title:'React file managment',
-        link:'https://rainbow-daifuku-05ddbf.netlify.app'
+        link:'https://rainbow-daifuku-05ddbf.netlify.app',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur laboriosam sunt nemo nulla aut itaque obcaecati ratione ex, iste animi illo veniam rerum rem sed optio officia suscipit minima tempora enim perferendis ipsa? Sapiente, reprehenderit maiores possimus optio maxime molestiae!',
+        // gitHubLink:''
     },
     {
         img:"https://i.imgur.com/TSb4hsY.png",
         title:'React movie app',
-        link:'https://movie-app-ten-cyan-76.vercel.app/'
+        link:'https://movie-app-ten-cyan-76.vercel.app/',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur laboriosam sunt nemo nulla aut itaque obcaecati ratione ex, iste animi illo veniam rerum rem sed optio officia suscipit minima tempora enim perferendis ipsa? Sapiente, reprehenderit maiores possimus optio maxime molestiae!',
+        gitHubLink:'https://github.com/behruzchick/movie-app'
     },
     {
         img:"https://i.imgur.com/zrcsxho.png",
         title:'Rest Countries app',
-        link:'https://tangerine-seahorse-aa7973.netlify.app'
+        link:'https://tangerine-seahorse-aa7973.netlify.app',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur laboriosam sunt nemo nulla aut itaque obcaecati ratione ex, iste animi illo veniam rerum rem sed optio officia suscipit minima tempora enim perferendis ipsa? Sapiente, reprehenderit maiores possimus optio maxime molestiae!',
+        gitHubLink:'https://github.com/behruzchick/rest-countries-app'
     },
     {
         img:"https://i.imgur.com/5D7yGUs.png",
         title:'Crypto App',
-        link:'https://taupe-gecko-0efb43.netlify.app'
+        link:'https://taupe-gecko-0efb43.netlify.app',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur laboriosam sunt nemo nulla aut itaque obcaecati ratione ex, iste animi illo veniam rerum rem sed optio officia suscipit minima tempora enim perferendis ipsa? Sapiente, reprehenderit maiores possimus optio maxime molestiae!',
+
     },
     {
         img:"https://i.imgur.com/IfFg2rl.png",
         title:'Blog wen app',
-        link:'https://funny-squirrel-c90649.netlify.app'
+        link:'https://funny-squirrel-c90649.netlify.app',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur laboriosam sunt nemo nulla aut itaque obcaecati ratione ex, iste animi illo veniam rerum rem sed optio officia suscipit minima tempora enim perferendis ipsa? Sapiente, reprehenderit maiores possimus optio maxime molestiae!'
     },
 ]
 
--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,20 +1,34 @@
 import { useGSAP } from '@gsap/react'
-import { Box, Container, ImageList, ImageListItem, ImageListItemBar, ToggleButton, ToggleButtonGroup } from '@mui/material'
+import { Box, Button, Container, ImageList, ImageListItem, ImageListItemBar, Modal, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
 import gsap from 'gsap'
 import React, { useState } from 'react'
 import './Projects.css'
+import CloseIcon from '@mui/icons-material/Close';
 import { projects } from "./AllProjects";
 import behruz from '../images/beha.png'
 import { useNavigate } from 'react-router-dom'
 const Projects = ({ classNameCom }) => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [modalItem,setModalItem]= useState({});
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const handleOpen = (item) => {
+    console.log("itemm",item);
+    setModalItem(item)
+    setOpen(true);
+  }
+
   useGSAP(() => {
     gsap
       .timeline()
-      .from('.projects-header', { x: '-500', duration: 1.3 ,opacity:0})
-      gsap
+      .from('.projects-header', { x: '-500', duration: 1.3, opacity: 0 })
+    gsap
       .timeline()
-      .from('.projects-list', { y: '100', duration: 1.3 ,opacity:0})
+      .from('.projects-list', { y: '100', duration: 1.3, opacity: 0 })
   })
   const [alignment, setAlignment] = useState('web');
 
@@ -25,40 +39,50 @@ const Projects = ({ classNameCom }) => {
     <Container className='projects'>
       <Box className='projects-header'>
         <h3 style={{ color: "white" }}>PROJECTS</h3>
-        {/* <ToggleButtonGroup
-          color="secondary"
-          value={alignment}
-          exclusive
-          onChange={handleChange}
-          aria-label="Platform"
-        >
-          <ToggleButton style={{ color: "gray" }} value={'studiyng projects'}>Studiyng projects</ToggleButton>
-          <ToggleButton style={{ color: "gray" }} value={'real projects'}>Real projects</ToggleButton>
-        </ToggleButtonGroup> */}
       </Box>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className='modal-box'>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {modalItem.title}
+            <CloseIcon onClick={handleClose} className='close-icon'/>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {modalItem.description}
+          </Typography>
+          <Typography sx={{mt:3}}>
+            <Button href={modalItem.link} target='_blank'>View demo</Button>
+            <Button href={modalItem.gitHubLink} target='_blank'>Github source</Button>
+          </Typography>
+        </Box>
+      </Modal>
       <Box className='projects-list'>
-          <ImageList className='image-list'  style={{borderRadius:"10px"}}>
-            {
-              projects.map((item,index) => (
-                <ImageListItem className='image-block' key={index} >
-                  <img
-                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                    src={`${item.img}?w=248&fit=crop&auto=format`}
-                    alt={item.title}
-                    style={{borderRadius:"10px",cursor:"pointer"}}
-                    onClick={() => window.open(item.link, '_blank')}
-                    loading="lazy"      
-                  />
+        <ImageList className='image-list' style={{ borderRadius: "10px" }}>
+          {
+            projects.map((item, index) => (
+              <ImageListItem className='image-block' key={index} onClick={() => handleOpen(item)}>
+                <img
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  alt={item.title}
+                  style={{ borderRadius: "10px", cursor: "pointer" }}
+                  // onClick={() => window.open(item.link, '_blank')}
+                  loading="lazy"
+                />
 
-                  <ImageListItemBar
-                    title={item.title}
-                    sx={{color:"white"}}
-                    position="below"
-                  />
-                </ImageListItem>
-              ))
-            }
-          </ImageList>
+                <ImageListItemBar
+                  title={item.title}
+                  sx={{ color: "white" }}
+                  position="below"
+                />
+              </ImageListItem>
+            ))
+          }
+        </ImageList>
       </Box>
     </Container>
   )
